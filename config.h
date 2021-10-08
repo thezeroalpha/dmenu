7e5f3b3e2cc88d5363f9e951862f422b78cf40e2
Alex Balgavy <alex@balgavy.eu>
2021-10-08 13:54:01 +0200

    Show colored icons

--- config.h
+++ config.h
@@ -4,7 +4,9 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"MesloLGS NF:size=10"
+	"MesloLGS NF:size=10",
+        "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true",
+        "Unifont:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
