c35b4dbe73e4649dc0cc3bbc3f5f4448b8e06e3b
Alex Balgavy <alexander.balgavy@spaceapplications.com>
2024-02-09 20:59:04 +0100

    Use my own colors

--- config.h
+++ config.h
@@ -1,6 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
-#include "/home/alex/.cache/wal/colors-wal-dmenu.h"
+// #include "/home/alex/.cache/wal/colors-wal-dmenu.h"
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
